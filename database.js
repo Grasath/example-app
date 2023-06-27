0865a9b56d96fd3048a74dffaf67099ddfd93037
Grasath <misteridler06@gmail.com>
2023-06-27 10:19:19 +0530

    Update Database

--- database.js
+++ database.js
@@ -8,7 +8,8 @@ async function main(){
         host : process.env.mysql_host,
         user : process.env.mysql_user,
         password : process.env.mysql_password,
-        database : process.env.mysql_database
+        database : process.env.mysql_database,
+        connectTimeout : 30000
     }).promise();
 }
 
